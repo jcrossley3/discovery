175046e807782aadc955937ecaff58564b40918a
Jim Crossley <jim@crossleys.org>
2022-10-01 10:22:37 -0400

    Blink the matrix pattern using the dk's 4 LED's

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,8 +3,10 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use nrf52840_dk_bsp::hal::{prelude::*, timer::Timer};
-use nrf52840_dk_bsp::Board;
+use nrf52840_dk_bsp::{
+    hal::{pac::TIMER0, prelude::*, timer::Timer},
+    Board, Led,
+};
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
@@ -13,16 +15,18 @@ fn main() -> ! {
     rtt_init_print!();
 
     let mut nrf52 = Board::take().unwrap();
-
     let mut timer = Timer::new(nrf52.TIMER0);
-
+    rprintln!("blinking");
     loop {
-        nrf52.leds.led_2.disable();
-        rprintln!("dark!");
-        timer.delay_ms(1_000_u16);
-
-        nrf52.leds.led_2.enable();
-        rprintln!("light!");
-        timer.delay_ms(1_000_u16);
+        blink(&mut nrf52.leds.led_1, &mut timer);
+        blink(&mut nrf52.leds.led_2, &mut timer);
+        blink(&mut nrf52.leds.led_4, &mut timer);
+        blink(&mut nrf52.leds.led_3, &mut timer);
     }
+}
+
+fn blink(led: &mut Led, t: &mut Timer<TIMER0>) {
+    led.enable();
+    t.delay_ms(100_u16);
+    led.disable();
 }
