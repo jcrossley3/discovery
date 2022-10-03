b81b85afc34f6e57a7c51143ade5a18b9b403985
Jim Crossley <jim@crossleys.org>
2022-10-03 19:37:09 -0400

    Send data to minicom from the dk
    Adapted the 07-uart example to the nRF52840-dk's CDC UART

--- microbit/src/07-uart/src/main.rs
+++ microbit/src/07-uart/src/main.rs
@@ -1,57 +1,25 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
-
-#[cfg(feature = "v1")]
-use microbit::{
-    hal::prelude::*,
-    hal::uart,
-    hal::uart::{Baudrate, Parity},
-};
-
-#[cfg(feature = "v2")]
-use microbit::{
-    hal::prelude::*,
-    hal::uarte,
-    hal::uarte::{Baudrate, Parity},
-};
-
-#[cfg(feature = "v2")]
-mod serial_setup;
-#[cfg(feature = "v2")]
-use serial_setup::UartePort;
+use rtt_target::rtt_init_print;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let board = microbit::Board::take().unwrap();
+    let board = nrf52840_dk_bsp::Board::take().unwrap();
 
-    #[cfg(feature = "v1")]
-    let mut serial = {
-        uart::Uart::new(
-            board.UART0,
-            board.uart.into(),
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        )
-    };
+    let mut serial = board.cdc;
 
-    #[cfg(feature = "v2")]
-    let mut serial = {
-        let serial = uarte::Uarte::new(
-            board.UARTE0,
-            board.uart.into(),
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        );
-        UartePort::new(serial)
-    };
+    // write a byte array
+    let buf: [u8; 1] = [b'X'; 1];
+    serial.write(&buf).unwrap();
 
-    nb::block!(serial.write(b'X')).unwrap();
-    nb::block!(serial.flush()).unwrap();
+    // write a string since the UART impls Write
+    let s = "The quick brown fox jumps over the lazy dog";
+    write!(serial, "\r\n{}\r\n", s).unwrap();
 
     loop {}
 }
